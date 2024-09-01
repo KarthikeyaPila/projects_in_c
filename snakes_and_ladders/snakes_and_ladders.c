b85c02952198ef4295d50d2087ed93d58307f960
Josh Kumar Polamarasetti <josh.kumar@ballys.com>
2024-09-01 16:18:42 +0100

    fix(josh-00): fix snake and ladder game

--- snakes_and_ladders/snakes_and_ladders.c
+++ snakes_and_ladders/snakes_and_ladders.c
@@ -1,111 +1,65 @@
 #include <stdio.h>
-/* #include <windows.h> */
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
-/* int update_pos_if_ladder_snake(int player_position, int dice)
-{
-    player_position = player_position + dice;
-    // check_ladder;
-    int ladders[16][2] = {
-        {4, 14},
-        {9, 31},
-        {20, 38},
-        {28, 84},
-        {40, 59},
-        {51, 67},
-        {63, 81},
-        {71, 91},
-        {17, 7},
-        {54, 34},
-        {62, 19},
-        {64, 60},
-        {87, 24},
-        {93, 73},
-        {95, 75},
-        {99, 2}};
+int update_pos_if_ladder_snake(int player_position) {
+  int ladders[16][2] = {{4, 14},  {9, 31},  {20, 38}, {28, 84},
+                        {40, 59}, {51, 67}, {63, 81}, {71, 91},
+                        {17, 7},  {54, 34}, {62, 19}, {64, 60},
+                        {87, 24}, {93, 73}, {95, 75}, {99, 2}};
 
-    for (int j = 0; j < 8; j++)
-    {
-        if (player_position == ladders[j][0])
-        {
-            if(j<9){printf("lets goo, you got up a ladder! \n");}
-            if(j>8){printf("you got eaten by a snake :( \n");}
-            player_position = ladders[j][1];
-        }
-
+  for (int i = 0; i < 16; i++) {
+    if (player_position == ladders[i][0]) {
+      return ladders[i][1];
     }
-} 
-*/
+  }
 
-int main()
-{
+  return player_position;
+}
 
-    int no_of_players = 1;
-    int trash;
-    printf("Lets playy snakes and ladderss. (i hope yk the rules lol). \n");
-    printf("Lets start the game! \n");
-    /* printf("how many players are there?? \n");
-    scanf("%d", &no_of_players); */
+int main() {
 
-    int *player_position_arr = calloc(no_of_players, sizeof(int));
+  int no_of_players = 1;
+  char trash;
+  printf("Lets playy snakes and ladderss. (i hope yk the rules lol). \n");
+  printf("Lets start the game! \n");
+  /* printf("how many players are there?? \n");
+  scanf("%d", &no_of_players); */
 
-    printf("\nokayy, lets start the game. \n");
+  int *player_position_arr = calloc(no_of_players, sizeof(int));
 
-    printf("you are currently at: %d\n", player_position_arr[0]);
+  printf("\nokayy, lets start the game. \n");
 
-    printf("you are currently at: %d\n", player_position_arr[0]);
+  printf("you are currently at: %d\n", player_position_arr[0]);
 
-    while (player_position_arr[0] != 100 || player_position_arr[0] < 100)
-    {
-        printf("\nroll the dice by pressing enter\n");
-        scanf("%c", &trash);
-        srand(time(NULL));
-        int dice = (rand() % 6) + 1;
-        printf("dice value: %d\n", dice);
-        {
-            {
-                player_position_arr[0] = player_position_arr[0] + dice;
-                // check_ladder;
-                int ladders[16][2] = {
-                    {4, 14},
-                    {9, 31},
-                    {20, 38},
-                    {28, 84},
-                    {40, 59},
-                    {51, 67},
-                    {63, 81},
-                    {71, 91},
-                    {17, 7},
-                    {54, 34},
-                    {62, 19},
-                    {64, 60},
-                    {87, 24},
-                    {93, 73},
-                    {95, 75},
-                    {99, 2}};
+  printf("you are currently at: %d\n", player_position_arr[0]);
 
-                for (int j = 0; j < 16; j++)
-                {
-                    if (player_position_arr[0] == ladders[j][0])
-                    {
-                        if (j < 9)
-                        {
-                            printf("lets goo, you got up a ladder! \n");
-                        }
-                        if (j > 8)
-                        {
-                            printf("you got eaten by a snake :( \n");
-                        }
-                        player_position_arr[0] = ladders[j][1];
-                    }
-                }
-                printf("your position is at: %d\n", player_position_arr[0]);
-            }
-            if (player_position_arr[0] == 100)
-            {
-                printf("congratss you won!!");
-            }
-        }
+  while (player_position_arr[0] != 100 || player_position_arr[0] < 100) {
+    printf("\nroll the dice by pressing enter\n");
+
+    scanf("%c", &trash);
+    srand(time(NULL));
+    int dice = (rand() % 6) + 1;
+    printf("dice value: %d\n", dice);
+
+    int player_position = player_position_arr[0] + dice;
+    if (player_position > 100) {
+      player_position = player_position_arr[0];
     }
+
+    int updated_player_position = update_pos_if_ladder_snake(player_position);
+    player_position_arr[0] = updated_player_position;
+
+    if (updated_player_position > player_position) {
+      printf("lets goo, you got up a ladder! \n");
+    } else if (updated_player_position < player_position) {
+      printf("you got eaten by a snake :( \n");
+    }
+
+    printf("your position is at: %d\n", updated_player_position);
+
+    if (updated_player_position == 100) {
+      printf("congratss you won!!");
+    }
+  }
 }
