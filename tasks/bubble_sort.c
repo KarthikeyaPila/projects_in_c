920c83717f0f9606508efba915cf99a41c9f2f48
Josh Kumar Polamarasetti <josh.kumar@ballys.com>
2025-03-01 15:08:33 +0000

    refactor(bubble-sort): create tests and header files

--- tasks/bubble_sort.c
+++ tasks/bubble_sort.c
@@ -1,30 +1,13 @@
-#include <stdio.h> 
-#include <assert.h>
-
-int main() {
-
-    int arr[] = {9,3,4,67,12,3,6,2,3,6};
-
-    int len = sizeof(arr)/sizeof(arr[0]);
-
-// bubble sort
-
-for (int i=0; i<len-1; i++) {
-    for (int j=0; j<len-i-1; j++) {
-        if (arr[j] > arr[j + 1]) {
-            int a = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = a;
+int* bubble_sort(int len, int* arr) {
+    for (int i=0; i<len-1; i++) {
+        for (int j=0; j<len-i-1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int a = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = a;
+            }
         }
-        assert(arr[j] <= arr[j+1]);
     }
-}
 
-// print
-for (int i=0; i<len; i++) {    
-    printf("%d ", arr[i]);
-}
-
-return 0;
-
+    return arr;
 }
